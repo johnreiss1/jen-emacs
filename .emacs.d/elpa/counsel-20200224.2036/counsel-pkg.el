73741c4ad0af5790eab0e267b3e6f4f0e0c8290f
Jen Reiss <johnwilliam.reiss@gmail.com>
2020-02-29 14:54:19 -0600

    Config Directory + Submodules

---
+++ .emacs.d/elpa/counsel-20200224.2036/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20200224.2036" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "0cff3e1577b8d4f2f9a9c69f0ab7d5ac674025e7" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ .emacs.d/elpa/swiper-20200227.1405/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20200227.1405" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "0cff3e1577b8d4f2f9a9c69f0ab7d5ac674025e7" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
